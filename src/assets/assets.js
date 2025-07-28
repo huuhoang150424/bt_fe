6e9450d55c749d498a0f52c1b6156bbbb3133279
hoang cuong <vancuong25112003@gmail.com>
2025-07-28 20:32:28 +0700

    feat: thêm trang news

---
+++ src/assets/assets.js
@@ -0,0 +1,62 @@
+export const posts = [
+    {
+        title: "Regional Manager limited time",
+        description:
+            "Rapidously repurpose leading edge growth strategies with just in time web readiness service...",
+        author: "Monamedia",
+        date: "30/05/2024",
+        comments: 0,
+        category: "IT Solution",
+        image: "../../public/blog-s-1-1.jpg",
+    },
+    {
+        title: "Unsatiable entreaties may",
+        description:
+            "Objectively communicate timely meta services for synergistic initiatives functionalities...",
+        author: "Monamedia",
+        date: "30/05/2024",
+        comments: 0,
+        category: "UI/UX Design",
+        image: "../../public/blog-s-1-1.jpg",
+    },
+]
+
+export const categories = [
+    "Cloud Solution",
+    "IT Solution",
+    "Network Marketing",
+    "SEO Marketing",
+    "UI/UX Design",
+    "Web Development",
+]
+
+export const recentPosts = [
+    {
+        title: "Regional Manager Limited Time",
+        date: "Th5 30 2024",
+        thumb: "../../public/blog-s-1-1.jpg",
+    },
+    {
+        title: "Unsatiable Entreaties May",
+        date: "Th5 30 2024",
+        thumb: "../../public/blog-s-1-1.jpg",
+    },
+    {
+        title: "Unsatiable Entreaties May Collecting Power.",
+        date: "Th2 13 2023",
+        thumb: "../../public/blog-s-1-1.jpg",
+    },
+]
+
+export const popularTags = [
+    "Advice",
+    "Agency",
+    "Business",
+    "Consultation",
+    "Design",
+    "Services",
+    "Software",
+    "Solution",
+    "Startup",
+    "Technology",
+]
