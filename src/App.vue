38f011ac82b515745bddd19a84c3de191b5c9748
huuhoang150424 <nguyenhoanghuu15042004@gmail.com>
2025-07-28 13:16:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="">
+    <h1 class="text-red-200"> hello</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
